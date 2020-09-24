d2530cf2c723ab3244cea3daae5860d4bef0c68b
shawnjonchan <xiaojiechen86@gmail.com>
2020-09-24 10:49:45 +0800

    need DOM

--- src/main.js
+++ src/main.js
@@ -3,37 +3,38 @@ var myChart = echarts.init(document.getElementById('skills'));
 
 // 指定图表的配置项和数据
 var option = {
-    tooltip: {},
-    radar: {
-        // shape: 'circle',
-        name: {
-            textStyle: {
-                color: '#fff',
-                backgroundColor: '#999',
-                borderRadius: 3,
-                padding: [3, 5]
-            }
-        },
-        indicator: [
-            { name: '静态页面', max: 100 },
-            { name: '编程基础', max: 100 },
-            { name: 'Vue 全家桶', max: 100 },
-            { name: 'React 全家桶', max: 100 },
-            { name: '项目开发', max: 100 },
-            { name: '沟通能力', max: 100 },
-        ]
+  title: {
+    text: "个人能力",
+  },
+ 
+  grid: {
+    left: "2%",
+    right: "4%",
+    bottom: "6%",
+    containLabel: true,
+  },
+  xAxis: {
+    type: "value",
+    boundaryGap: [0, 0.01],
+  },
+  yAxis: {
+    type: "category",
+    data: [
+      "项目开发",
+      "沟通能力",
+      "编程基础",
+      "React 全家桶",
+      "Vue 全家桶",
+      "静态页面",
+    ],
+  },
+  series: [
+    {
+      name: "能力表",
+      type: "bar",
+      data: [80, 70, 85, 80, 90, 95],
     },
-    series: [{
-        name: '预算 vs 开销（Budget vs spending）',
-        type: 'radar',
-        // areaStyle: {normal: {}},
-        data: [
-            {
-                value: [100, 70, 90, 80, 85, 70],
-                name: '能力雷达'
-            }
-        ]
-    }]
+  ],
 };
 
 // 使用刚指定的配置项和数据显示图表。
